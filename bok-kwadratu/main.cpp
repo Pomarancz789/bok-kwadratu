5b9365405546364c8d695e009adef56fb1723979
Pomarancz789 <granat-02krawcy@icloud.com>
2022-02-23 09:53:06 +0100

    Initial Commit

---
+++ bok-kwadratu/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bok-kwadratu
+//
+//  Created by Jakub Wtorkiewicz on 23/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
